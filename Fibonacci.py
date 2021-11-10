f61b45f58f7832855de48f7bb7180a5c6ec7f620
Venkateshs1 <93922959+Venkateshs1@users.noreply.github.com>
2021-11-10 18:19:12 +0530

    Add files via upload

---
+++ Fibonacci.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+a,b=0,1
+
+while b<50:
+    print(b,end=" ")
+    a,b = b,a+b
+
---
+++ Reverse method 1 (1).py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+word = input()
+result = word[::-1]
+print(result)
+
+
+# In[ ]:
+
+
+
+
