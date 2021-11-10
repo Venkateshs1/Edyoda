3a62b17ef2e38c8664cef500253c7398f7c52b76
Venkateshs1 <93922959+Venkateshs1@users.noreply.github.com>
2021-11-10 18:31:09 +0530

    Add files via upload

---
+++ Reverse method.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Write a Python program that accepts a word from the user and reverse it.
+
+word = input()
+result = word[::-1]
+print(result)
+
+
+# In[ ]:
+
+
+
+
