212cdc8de462a7366f8499f769599494e9ea94e1
Venkateshs1 <93922959+Venkateshs1@users.noreply.github.com>
2021-11-10 18:27:03 +0530

    Add files via upload

---
+++ Fibonacciproblem .py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#Write a Python program to get the Fibonacci series between 0 to 50
+
+
+a,b=0,1
+
+while b<50:
+    print(b,end=" ")
+    a,b = b,a+b
+
