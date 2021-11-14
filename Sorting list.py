7abb7d7e2f3cd13fcdbb31017cb44642192449fa
Venkateshs1 <93922959+Venkateshs1@users.noreply.github.com>
2021-11-14 18:34:21 +0530

    Add files via upload

---
+++ Sorting list.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+tuple1 = [(2,5), (1,2), (4,4), (2,3), (2,1)]
+list2 = []
+list3 = []
+
+for t in tuple1:
+    list2.append(t[1],)
+    
+list2.sort()
+
+for l in list2:
+    for q in tuple1:
+        if l == int(q[1],):
+            list3.append(q)
+            
+print(list3)
+
+
+# In[ ]:
+
+
+
+
