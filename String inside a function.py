c84b402a21fbed6eed7f29d4423acac27b4af8ee
Venkateshs1 <93922959+Venkateshs1@users.noreply.github.com>
2021-11-21 19:00:45 +0530

    Add files via upload

---
+++ String inside a function.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+'''
+Write a Python program to reverse a string.
+
+﻿Sample String : "1234abcd"
+
+Expected Output : "dcba4321"
+'''
+
+def string_reverse(str1):
+
+    rstr1 = ''
+    index = len(str1)
+    while index > 0:
+        rstr1 += str1[ index - 1 ]
+        index = index - 1
+    return rstr1
+print(string_reverse('1234abcd'))
+
+
+# In[ ]:
+
+
+
+
