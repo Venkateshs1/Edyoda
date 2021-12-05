9691c2fbaffc8ce955ae77ac476d8c68a31c9f42
Venkateshs1 <93922959+Venkateshs1@users.noreply.github.com>
2021-12-05 17:19:35 +0530

    Add files via upload

---
+++ Find the Way eith Maps.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+'''Write a Python program to triple all numbers of a given list of integers. Use Python map.
+
+
+sample list: [1, 2, 3, 4, 5, 6, 7]'''
+
+nums = (1, 2, 3, 4, 5, 6, 7) 
+print("Original list: ", nums)
+result = map(lambda x: x + x + x, nums) 
+print("\nTriple of said list numbers:")
+print(list(result))
+
+
+# In[ ]:
+
+
+
+
